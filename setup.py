f156a245a2043a1f8557de1014bac45e4724cfc6
Bermd <54647267+bernd-lab@users.noreply.github.com>
2025-04-10 14:24:55 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,5 +11,5 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/nathanrchn/perplexityai",
     packages=find_packages(),
-    requires=["requests", "websocket-client"]
+    install_requires=["websocket-client>=1.2.0", "requests>=2.20.0"]
 )
